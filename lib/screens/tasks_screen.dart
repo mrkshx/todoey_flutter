27e8ee27e7a7f127c122fb592f1d48e1903eaf37
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-04 20:26:00 +0200

    added icon and text

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: [Icon(Icons.list), Text('Todoey')],
+      ),
+    );
   }
 }

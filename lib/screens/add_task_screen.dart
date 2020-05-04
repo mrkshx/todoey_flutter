69bed1c70b6bed226adca972098672f904bde489
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-05 00:01:58 +0200

    added Column with text, textfield and flatbutton to container

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatelessWidget {
@@ -6,12 +7,47 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20),
             topRight: Radius.circular(20),
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, color: Colors.lightBlueAccent),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 4, color: Colors.lightBlueAccent),
+                ),
+              ),
+              keyboardType: TextInputType.text,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            FlatButton(
+              onPressed: () {
+                // Nothing to do yet
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightBlueAccent,
+            )
+          ],
         ),
       ),
     );

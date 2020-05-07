ce67da224abb500b7c8e14069aec5cdf07fb9c5e
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-07 16:05:37 +0200

    readded functionality for onChanged

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
+  final Function checkboxCallback;
 
-  TaskTile({this.isChecked, this.taskTitle});
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +19,8 @@ class TaskTile extends StatelessWidget {
       trailing: Checkbox(
         activeColor: Colors.lightBlueAccent,
         value: isChecked,
-//        onChanged: toggleCheckboxState,
+        onChanged: checkboxCallback,
       ),
     );
   }
 }
-
-//(bool checkboxState) {
-//setState(() {
-//isChecked = checkboxState;
-//});
-//},

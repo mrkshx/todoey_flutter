e90d02859aa75cbe0cc7921516611f828c36e2c6
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-07 15:48:36 +0200

    added new variable taskTitle and initialize this and isChecked

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
-  final bool isChecked = false;
+  final bool isChecked;
+  final String taskTitle;
+
+  TaskTile({this.isChecked, this.taskTitle});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'Buy avocados',
+        taskTitle,
         style: TextStyle(
           decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
       trailing: Checkbox(
         activeColor: Colors.lightBlueAccent,
-        value: checkboxState,
+        value: isChecked,
 //        onChanged: toggleCheckboxState,
       ),
     );
   }
 }
-
 
 //(bool checkboxState) {
 //setState(() {

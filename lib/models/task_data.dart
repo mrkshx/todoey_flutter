5cb9eaf9af33e127c13a52282d3119185d2a0714
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-13 16:41:51 +0200

    made tasks private and created a getter function with the help of unmodifiablelistview

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,20 +1,26 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy paprika'),
     Task(name: 'Buy banana'),
     Task(name: 'Buy peanuts'),
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
   }
 
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 }

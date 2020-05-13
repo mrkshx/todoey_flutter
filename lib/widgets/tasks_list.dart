01aa68fdea322753381a2c79a863fdb07d1d5724
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-13 14:55:42 +0200

    wrapped ListView.build into Consumer

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,19 +6,23 @@ import 'package:todoeyflutter/models/task_data.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          checkboxCallback: (checkboxState) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              checkboxCallback: (checkboxState) {
 //            setState(() {
 //              Provider.of<TaskData>(context).tasks[index].toggleDone();
 //            });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }

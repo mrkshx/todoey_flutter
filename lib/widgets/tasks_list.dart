a804697e1e2cc7b54533e8a36647cd943838939b
Markus Mengel <markus.mengelhx@gmail.com>
2020-10-07 00:59:12 +0200

    wrapped SwipeActionCell around TaskTile

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'task_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:todoeyflutter/models/task_data.dart';
+import 'package:flutter_swipe_action_cell/flutter_swipe_action_cell.dart';
 
 class TasksList extends StatelessWidget {
   @override
@@ -11,15 +12,26 @@ class TasksList extends StatelessWidget {
         return ListView.builder(
           itemBuilder: (context, index) {
             final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.name,
-              isChecked: task.isDone,
-              checkboxCallback: (checkboxState) {
-                taskData.updateTask(task);
-              },
-              longPressCallback: () {
-                taskData.removeTask(task);
-              },
+            return SwipeActionCell(
+              key: ObjectKey(task),
+              trailingActions: <SwipeAction>[
+                SwipeAction(
+                    title: 'Delete',
+                    onTap: (CompletionHandler handler) async {
+                      await handler(true);
+                      taskData.removeTask(task);
+                    })
+              ],
+              child: TaskTile(
+                taskTitle: task.name,
+                isChecked: task.isDone,
+                checkboxCallback: (checkboxState) {
+                  taskData.updateTask(task);
+                },
+                longPressCallback: () {
+                  taskData.removeTask(task);
+                },
+              ),
             );
           },
           itemCount: taskData.taskCount,
